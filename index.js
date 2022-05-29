3e9174cead07b6cf3193a890dae3f6df4bd09caa
Niraj Singh <102036558+Starniraj@users.noreply.github.com>
2022-05-29 18:11:01 +0530

    Update index.js

--- index.js
+++ index.js
@@ -133,24 +133,3 @@
 //     butt.onclick= hidetext
 // }
 
-
-const user = {
-  email: "e@mail.com",
-  password: "12345"
-}
-
-const updateUser = ({ email, password }) => {
-  if (email) {
-          Object.assign(user, { email })
-  }
-
-if (password) {
-      user.password = password
-}
-
-return user
-}
-
-const updatedUser = updateUser({ email: "new@email.com" })
-
-console.log(updatedUser === user)
